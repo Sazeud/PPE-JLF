012075e027f90989f04c7bf20678ebad8c604863
Sazeud <66422524+Sazeud@users.noreply.github.com>
2020-11-02 16:27:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,51 @@
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-    <form class="form-inline my-2 my-lg-0">
-      <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
-    </form>
-  </div>
-</nav>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	    </ul>
+	    <span class="navbar-text">
+	      Navbar text with an inline element
+	    </span>
+	    <form class="form-inline my-2 my-lg-0">
+	      <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+	    </form>
+	  </div>
+	</nav>
+	<br><br><br><br>
+
+	 <div class="container">
+	      <h1>Tableaux des prochaines liaisons</h1>
+	      <table class="table">
+		<thead>
+		  <tr>
+		    <th>Nom de liaison</th>
+		    <th>Départ</th>
+		    <th>Arrivée</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+		    <td><a href="#">L34</a></td>
+		    <td>Quiberon</td>
+		    <td>Le Palais</td>
+		  </tr>
+		  <tr>
+		    <td><a href="#">L23</a></td>
+		    <td>Lorient</td>
+		    <td>Port-Tudy</td>
+		  </tr>
+		  <tr>
+		    <td><a href="#">L45</a></td>
+		    <td>Vannes</td>
+		    <td>Le Palais</td>
+		  </tr>
+		  <tr>
+          	<td><a href="#">L2</a></td>
+          	<td>Sauzon</td>
+          	<td>Quiberon</td>
+        </tbody>
+      </table>
+    </div>
   </body> 
 </html>
